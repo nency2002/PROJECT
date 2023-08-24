337dbd81dc22e4946586b5b87836f5ba65e2076b
nency kotadiya <kotadiyanency7@gmail.com>
2023-08-24 06:13:54 +0530

    add to cart

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,9 +1,121 @@
 import Navbar from "../components/nav.js";
-document.getElementById('navbar').innerHTML=Navbar();
+document.getElementById('navbar').innerHTML = Navbar();
+let totalprice = 0;
 
+const display = (cart) => {
 
-fetch(`http://localhost:3000/cart`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-            })
+    cart.map((products) => {
+        
+
+        let maindiv = document.createElement("tr");
+
+        let imgdiv = document.createElement("td");
+        let image = document.createElement("img");
+        image.src = products.image;
+
+        let texts = document.createElement("td");
+        let title = document.createElement("td");
+        title.innerHTML = products.title;
+
+        let prices = document.createElement("td");
+        let price1 = document.createElement("td");
+        price1.innerHTML = products.price;
+
+        let plusminse = document.createElement("td");
+
+        let minse = document.createElement("button");
+        minse.innerHTML = "-";
+        minse.addEventListener("click",()=>{
+            console.log(products.id)
+            let loggin = localStorage.getItem("loggin")
+            if(loggin){
+                fetch(`http://localhost:3000/cart?id=${products.id}`)
+                .then((res)=>res.json())
+                .then((data)=>{
+                    if(data.length > 0){
+                        fetch (`http://localhost:3000/cart/${products.id}`,{
+                            method:"PATCH",
+                            headers:{"content-type":"application/json"},
+                            body:JSON.stringify({Qty:data[0].Qty-1}),
+                        });
+                        console.log(data.length)
+                    }
+                    else{
+                        fetch ("http://localhost:3000/cart",{
+                            method:"POST",
+                            headers:{"content-type":"application/json"},
+                            body:JSON.stringify({...products,Qty:1}),
+                        });
+                    }
+                })
+            }
+            else{
+                alert("you have to loggin first")
+                setTimeout(() => {
+                    window.location.href = "/pages/login.html"
+                },1000)
+            }
+        })
+
+        let noq = document.createElement("button");
+        noq.innerHTML = products.Qty;
+
+        let plus = document.createElement("button");
+        plus.innerHTML = "+";
+        plus.addEventListener("click",()=>{
+            console.log(products.id)
+            let loggin = localStorage.getItem("loggin")
+            if(loggin){
+                fetch(`http://localhost:3000/cart?id=${products.id}`)
+                .then((res)=>res.json())
+                .then((data)=>{
+                    if(data.length > 0){
+                        fetch (`http://localhost:3000/cart/${products.id}`,{
+                            method:"PATCH",
+                            headers:{"content-type":"application/json"},
+                            body:JSON.stringify({Qty:data[0].Qty+1}),
+                        });
+                        console.log(data.length)
+                    }
+                    else{
+                        fetch ("http://localhost:3000/cart",{
+                            method:"POST",
+                            headers:{"content-type":"application/json"},
+                            body:JSON.stringify({...products,Qty:1}),
+                        });
+                    }
+                })
+            }
+            else{
+                alert("you have to loggin first")
+                setTimeout(() => {
+                    window.location.href = "/pages/login.html"
+                },1000)
+            }
+        })
+
+        let total = document.createElement("td");
+        total.innerHTML = totalprice += products.Qty * products.price ;
+
+        let btn = document.createElement("Button");
+        btn.innerHTML = "Delete"
+        btn.addEventListener("click",()=>{
+            fetch (`http://localhost:3000/cart/${products.id}`,{
+            method:"DELETE",
+        })
+        })
+        imgdiv.append(image);
+        texts.append(title);
+        prices.append(price1);
+        plusminse.append(minse, noq, plus);
+        maindiv.append(imgdiv, texts, prices, plusminse ,total , btn);
+        document.getElementById("product").append(maindiv);
+    });
+};
+
+const get = () => {
+    fetch("http://localhost:3000/cart")
+        .then((response) => response.json())
+        .then((response) => display(response))
+}
+get()
--- javascript/pages.js
+++ javascript/pages.js
@@ -1,61 +1,88 @@
 import Navbar from "../components/nav.js";
-document.getElementById('navbar').innerHTML=Navbar();
+document.getElementById('navbar').innerHTML = Navbar();
 
-const display =(data) =>{
-    data.map((products)=>{
+const display = (data) => {
+    data.map((products) => {
         let img = document.createElement('img');
-        img.src=products.image;
+        img.src = products.image;
         let title = document.createElement('h2');
-        title.innerHTML=products.title;
+        title.innerHTML = products.title;
         let price = document.createElement('p');
-        price.innerHTML=products.price;
+        price.innerHTML = products.price;
         let category = document.createElement('p');
-        category.innerHTML=products.category;
+        category.innerHTML = products.category;
         let rating = document.createElement('span');
-        rating.innerHTML=products.rating.rate;
-        if(products.rating.rate > 4){
-            rating.style.color="green"
+        rating.innerHTML = products.rating.rate;
+        if (products.rating.rate > 4) {
+            rating.style.color = "green"
         }
-        else if((products.rating.rate < 4) && (products.rating.rate >= 3)){
-            rating.style.color="yellow"
-
+        else if ((products.rating.rate < 4) && (products.rating.rate >= 3)) {
+            rating.style.color = "yellow"
         }
-        else{
-            rating.style.color="red"
-
+        else {
+            rating.style.color = "red"
         }
         let btn = document.createElement('Button');
-        btn.innerHTML="BUY NOW";
-        btn.addEventListener("click",()=>{
-            // console.log(products.id)
+        btn.innerHTML = "BUY NOW";
+        // btn.addEventListener("click",()=>{
+        //     console.log(products.id)
+        //     let loggin = localStorage.getItem("loggin")
+        //     if(loggin){
+        //         fetch(`http://localhost:3000/cart?id=${products.id}`)
+        //         .then((res)=>res.json())
+        //         .then((data)=>{
+        //             if(data.length > 0){
+        //                 alert("product was defaine")
+        //                 fetch (`http://localhost:3000/cart/${products.id}`,{
+        //                     method:"PATCH",
+        //                     headers:{"content-type":"application/json"},
+        //                     body:JSON.stringify({Qty:data[0].Qty+1}),
+        //                 });
+        //                 console.log(data.length)
+        //             }
+        //             else{
+        //                 fetch ("http://localhost:3000/cart",{
+        //                     method:"POST",
+        //                     headers:{"content-type":"application/json"},
+        //                     body:JSON.stringify({...products,Qty:1}),
+        //                 });
+        //             }
+        //         })
+        //     }
+        //     else{
+        //         alert("you have to loggin first")
+        //         setTimeout(() => {
+        //             window.location.href = "/pages/login.html"
+        //         },1000)
+        //     }
+        // })
+        btn.addEventListener("click", () => {
+            console.log(products.id)
             let loggin = localStorage.getItem("loggin")
-            if(loggin){
-                fetch ("http://localhost:3000/cart",{
-                        method:"POST",
-                        headers:{"content-type":"application/json"},
-                        body:JSON.stringify(products),
-                    });
+            if (loggin) {
+                fetch("http://localhost:3000/cart", {
+                    method: "POST",
+                    headers: { "content-type": "application/json" },
+                    body: JSON.stringify(products),
+                });
             }
-            else{
+            else {
                 alert("you have to loggin first")
                 setTimeout(() => {
                     window.location.href = "/pages/login.html"
-                },1000)
+                }, 1000)
             }
         })
         let div = document.createElement('div');
-        div.append(img,title,price,category,rating,btn)
+        div.append(img, title, price, category, rating, btn)
         document.getElementById("box2").append(div)
-
-
-
     })
 }
 
 
-const get =()=>{
+const get = () => {
     fetch("http://localhost:3000/products")
-    .then((response)=>response.json())
-    .then((response)=>display(response))
+        .then((response) => response.json())
+        .then((response) => display(response))
 }
 get()
